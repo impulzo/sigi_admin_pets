81aa374ee45cc8af7c56ab07df3a12a596a489cb
Isaac Iglesias <eliwood86@hotmail.com>
2025-03-26 19:36:18 -0600

    se han acabado de colocar los botones faltantes

--- resources/views/vendor/voyager/medicalhistories/vaccine.blade.php
+++ resources/views/vendor/voyager/medicalhistories/vaccine.blade.php
@@ -1,12 +1,15 @@
 @extends('voyager::master')
 
-@section('page_title', 'Historial de movimientos')
+@section('page_title', 'Vacunas')
 
 @section('page_header')
     <h1 class="page-title">
         <i class="voyager-book"></i>
-        Historial de movimientos
+        Vacunas
     </h1>
+	<a href="{{ route('voyager.vaccines.create') }}" class="btn btn-success">
+    <i class="voyager-plus"></i> Crear
+	</a>
     @include('voyager::multilingual.language-selector')
 @stop
 
@@ -22,26 +25,44 @@
                                 <thead>
                                     <tr>
                                         <th>Nombre</th>
-                                        <th>Fecha de aplicaciòn</th>
-                                        <th>Siguiente aplicaciòn</th>
+                                        <th>Fecha de aplicación</th>
+                                        <th>Siguiente aplicación</th>
                                         <th>Dosis</th>
-                                        <th>¿Quièn la aplicò?</th>
+                                        <th>¿Quién la aplicó?</th>
                                         <th>Estatus</th>
-										<th>Detalles</th>
-										<th>Mascota</th>
+                                        <th>Detalles</th>
+                                        <th>Mascota</th>
+                                        <th>Acciones</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @foreach ($data as $item)
                                         <tr>
-											<td>{{ $item->type }}</td>
+                                            <td>{{ $item->type }}</td>
                                             <td>{{ $item->application_date }}</td>
                                             <td>{{ $item->next_application }}</td>
-											<td>{{ $item->dose }}</td>
-											<td>{{ $item->who_applied }}</td>
-											<td>{{ $item->status }}</td>
-											<td>{{ $item->details }}</td>
-											<td>{{ $item->pet ? $item->pet->name : '' }}</td>
+                                            <td>{{ $item->dose }}</td>
+                                            <td>{{ $item->who_applied }}</td>
+                                            <td>{{ $item->status }}</td>
+                                            <td>{{ $item->details }}</td>
+                                            <td>{{ $item->pet ? $item->pet->name : '' }}</td>
+                                            <td>
+                                                <form action="{{ route('voyager.vaccines.destroy', $item->id) }}" method="POST" class="delete-form" style="display:inline;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-sm btn-danger pull-right delete" style="margin-right: 5px">
+                                                        <i class="voyager-trash"></i> Borrar
+                                                    </button>
+                                                </form>
+
+                                                <a href="{{ url('/admin/vaccines/'.$item->id .'/edit/') }}" class="btn btn-sm btn-primary pull-right edit">
+                                                    <i class="voyager-edit"></i> Editar
+                                                </a>
+
+                                                <a href="{{ url('/admin/vaccines/'.$item->id) }}" class="btn btn-sm btn-warning pull-right view">
+                                                    <i class="voyager-eye"></i> Ver
+                                                </a>
+                                            </td>
                                         </tr>
                                     @endforeach
                                 </tbody>
@@ -55,50 +76,37 @@
 @stop
 
 @section('javascript')
-    <!-- DataTables -->
     <script>
-        $('#dataTable').DataTable({
-            destroy: false,
-            pageLength: 10,
-            order: [
-                [1, 'desc']
-            ], //ordenar de forma descendente
-            language: {
-                sProcessing: "Procesando...",
-                sLengthMenu: "Mostrar _MENU_ registros",
-                sZeroRecords: "No se encontraron resultados",
-                sEmptyTable: "Ningún dato disponible en esta tabla",
-                sInfo: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-                sInfoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
-                sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
-                sInfoPostFix: "",
-                sSearch: "Buscar:",
-                sUrl: "",
-                sInfoThousands: ",",
-                sLoadingRecords: "Cargando...",
-                oPaginate: {
-                    sFirst: "Primero",
-                    sLast: "Último",
-                    sNext: "Siguiente",
-                    sPrevious: "Anterior",
+        document.addEventListener("DOMContentLoaded", function () {
+            // Inicializar DataTable
+            $('#dataTable').DataTable({
+                destroy: false,
+                pageLength: 10,
+                order: [
+                    [1, 'desc']
+                ],
+                language: {
+                    sProcessing: "Procesando...",
+                    sLengthMenu: "Mostrar _MENU_ registros",
+                    sZeroRecords: "No se encontraron resultados",
+                    sEmptyTable: "Ningún dato disponible en esta tabla",
+                    sInfo: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                    sInfoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+                    sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
+                    sSearch: "Buscar:",
+                    sLoadingRecords: "Cargando...",
+                    oPaginate: {
+                        sFirst: "Primero",
+                        sLast: "Último",
+                        sNext: "Siguiente",
+                        sPrevious: "Anterior"
+                    },
                 },
-                oAria: {
-                    sSortAscending: ": Activar para ordenar la columna de manera ascendente",
-                    sSortDescending: ": Activar para ordenar la columna de manera descendente",
-                },
-            },
-            lengthMenu: [
-                [5, 10, 15],
-                [5, 10, 15],
-            ],
+                lengthMenu: [
+                    [5, 10, 15],
+                    [5, 10, 15],
+                ],
+            });
         });
-
-        function reload() {
-            var initial = $('#initial_date').val();
-            var final = $('#final_date').val();
-            if (initial != '' && final != '') {
-                window.location.href = "{{ url('/') }}/admin/reports/ingresos-gastos/" + initial + "/" + final;
-            }
-        }
     </script>
 @stop
--- resources/views/vendor/voyager/pets/medicalhistory.blade.php
+++ resources/views/vendor/voyager/pets/medicalhistory.blade.php
@@ -1,11 +1,11 @@
 @extends('voyager::master')
 
-@section('page_title', 'Historial de movimientos')
+@section('page_title', 'Historial Médico')
 
 @section('page_header')
     <h1 class="page-title">
         <i class="voyager-book"></i>
-        Historial de movimientos
+        Historial Médico
     </h1>
     @include('voyager::multilingual.language-selector')
 @stop
@@ -21,13 +21,12 @@
                             <table id="dataTable" class="table table-hover">
                                 <thead>
                                     <tr>
-                                        <th>Nombre</th>
+                                        <th>Mascota</th>
                                         <th>Motivo</th>
                                         <th>Desde cuando</th>
                                         <th>Sìntomas</th>
-                                        <th>Diagnòstico</th>
-                                        <th>Pronòstico</th>
 										<th>Tratamiento</th>
+										<th>Acciones</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -37,9 +36,26 @@
 											<td>{{ $item->reason }}</td>
                                             <td>{{ $item->since_when }}</td>
                                             <td>{{ $item->symptoms }}</td>
-											<td>{{ $item->diagnosis }}</td>
-											<td>{{ $item->forecast }}</td>
 											<td>{{ $item->treatment }}</td>
+											<td>
+												 <form action="{{ route('voyager.medical-histories.destroy', $item->id) }}" method="POST" class="delete-form" style="display:inline;">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-sm btn-danger pull-right delete" style="margin-right: 5px">
+                                                        <i class="voyager-trash"></i> Borrar
+                                                    </button>
+                                                </form>
+
+												<a href="{{ url('/admin/medical-histories/'.$item->id .'/edit/') }}" class="btn btn-sm btn-primary pull-right edit">
+												<i class="voyager-edit"></i>Editar</a>
+
+												<a href="{{ url('/admin/medical-histories/'.$item->id) }}" class="btn btn-sm btn-warning pull-right view">
+												<i class="voyager-eye"></i>Ver</a>
+
+												<a href="{{ url('/admin/medicalhistory/'.$item->pet_id) .'/vaccine/'}}" class="btn btn-sm btn-primary pull-right" style="margin-right: 5px">
+												<i class="voyager-file-text"></i>Vacunas</a>
+
+											</td>
                                         </tr>
                                     @endforeach
                                 </tbody>
@@ -53,14 +69,13 @@
 @stop
 
 @section('javascript')
-    <!-- DataTables -->
     <script>
         $('#dataTable').DataTable({
             destroy: false,
             pageLength: 10,
             order: [
                 [1, 'desc']
-            ], //ordenar de forma descendente
+            ],
             language: {
                 sProcessing: "Procesando...",
                 sLengthMenu: "Mostrar _MENU_ registros",
@@ -69,20 +84,13 @@
                 sInfo: "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
                 sInfoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
                 sInfoFiltered: "(filtrado de un total de _MAX_ registros)",
-                sInfoPostFix: "",
                 sSearch: "Buscar:",
-                sUrl: "",
-                sInfoThousands: ",",
                 sLoadingRecords: "Cargando...",
                 oPaginate: {
                     sFirst: "Primero",
                     sLast: "Último",
                     sNext: "Siguiente",
-                    sPrevious: "Anterior",
-                },
-                oAria: {
-                    sSortAscending: ": Activar para ordenar la columna de manera ascendente",
-                    sSortDescending: ": Activar para ordenar la columna de manera descendente",
+                    sPrevious: "Anterior"
                 },
             },
             lengthMenu: [
@@ -91,12 +99,13 @@
             ],
         });
 
-        function reload() {
-            var initial = $('#initial_date').val();
-            var final = $('#final_date').val();
-            if (initial != '' && final != '') {
-                window.location.href = "{{ url('/') }}/admin/reports/ingresos-gastos/" + initial + "/" + final;
-            }
-        }
+        // Confirmación antes de borrar
+        document.querySelectorAll('.delete-form').forEach(form => {
+            form.addEventListener('submit', function(event) {
+                if (!confirm('¿Estás seguro de que deseas eliminar este historial médico?')) {
+                    event.preventDefault();
+                }
+            });
+        });
     </script>
 @stop
