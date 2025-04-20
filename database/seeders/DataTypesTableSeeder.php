bda581e54bf1414176d9d6a7490de29f4c34726b
Isaac Iglesias <eliwood86@hotmail.com>
2025-04-19 18:25:10 -0600

    Se hicieron los seeders

--- database/seeders/DataTypesTableSeeder.php
+++ database/seeders/DataTypesTableSeeder.php
@@ -287,7 +287,7 @@ class DataTypesTableSeeder extends Seeder
                 'server_side' => 0,
                 'details' => '{"order_column":null,"order_display_column":null,"order_direction":"asc","default_search_key":null,"scope":null}',
                 'created_at' => '2024-11-04 21:09:10',
-                'updated_at' => '2025-04-18 23:27:03',
+                'updated_at' => '2025-04-19 17:27:25',
             ),
             15 => 
             array (
