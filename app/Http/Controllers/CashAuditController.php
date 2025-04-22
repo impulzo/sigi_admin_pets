526930aaad42b6f41a4b8762b25b43e5d3c1bd7a
Isaac Iglesias <eliwood86@hotmail.com>
2025-04-22 01:25:21 -0600

    se corrigieron breads 3

--- app/Http/Controllers/CashAuditController.php
+++ app/Http/Controllers/CashAuditController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use TCG\Voyager\Http\Controllers\VoyagerBaseController;
 use App\Traits\LogErrorTrait;
 use App\Models\Expense;
+use App\Models\Receipt;
 
 
 class CashAuditController extends VoyagerBaseController
@@ -15,42 +16,52 @@ class CashAuditController extends VoyagerBaseController
 	public function store(Request $request)
 	{
 		$totalExpenses = 0;
+		$totalReceipts = 0;
 
 		$startDate = $request->all()['start_date'];
 		$endDate = $request->all()['finish_date'];
 
 		$expenses = Expense::whereBetween('date', [$startDate, $endDate])->get();
-
+		$receipts = Receipt::whereBetween('date', [$startDate, $endDate])->get();
 
 		foreach ($expenses as $expense) {
 			$totalExpenses += $expense->amount;
 		}
 
+		foreach ($receipts as $receipt) {
+			$totalReceipts += $receipt->amount;
+		}
+
 		$request->merge([
 			'user_id' => \Auth::user()->id,
 			'total_bills' => $totalExpenses,
-			'total_ingress' => 0,
+			'total_ingress' => $totalReceipts,
 		]);
 		return parent::store($request);
 	}
 
 	public function update(Request $request, $id){
 		$totalExpenses = 0;
+		$totalReceipts = 0;
 
 		$startDate = $request->all()['start_date'];
 		$endDate = $request->all()['finish_date'];
 
 		$expenses = Expense::whereBetween('date', [$startDate, $endDate])->get();
-
+		$receipts = Receipt::whereBetween('date', [$startDate, $endDate])->get();
 
 		foreach ($expenses as $expense) {
 			$totalExpenses += $expense->amount;
 		}
 
+		foreach ($receipts as $receipt) {
+			$totalReceipts += $receipt->amount;
+		}
+
 		$request->merge([
 			'user_id' => \Auth::user()->id,
 			'total_bills' => $totalExpenses,
-			'total_ingress' => 0,
+			'total_ingress' => $totalReceipts,
 		]);
 
 		return parent::update($request, $id);
